3700518707d44955ac2e2b3789344f8b40b2862a
tngon462 <tngon462@gmail.com>
2025-09-30 20:08:55 +0900

    Update state.js

--- assets/js/state.js
+++ assets/js/state.js
@@ -1,4 +1,4 @@
-// Khôi phục trạng thái bàn sau reload
+// state.js
 document.addEventListener("DOMContentLoaded", () => {
   const tableId = localStorage.getItem("tableId");
   const tableUrl = localStorage.getItem("tableUrl");
@@ -7,6 +7,11 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("select-table").classList.add("hidden");
     document.getElementById("start-screen").classList.remove("hidden");
     document.getElementById("selected-table").textContent = tableId;
-    document.getElementById("start-order").setAttribute("data-url", tableUrl);
+    document
+      .getElementById("start-order")
+      .setAttribute("data-url", tableUrl);
+
+    // ✅ quan trọng: set global cho blackout.js
+    window.tableId = tableId;
   }
 });
