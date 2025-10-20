0d798e67ebcbe92ea022342ec6900f3cc82ac4d6
tngon462 <tngon462@gmail.com>
2025-10-20 14:49:53 +0900

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,5 +1,5 @@
 // assets/js/firebase.js
-(function () {
+(function(){
   const firebaseConfig = {
     apiKey: "AIzaSyB4u2G41xdGkgBC0KltleRpcg5Lwru2RIU",
     authDomain: "tngon-b37d6.firebaseapp.com",
@@ -7,31 +7,13 @@
     projectId: "tngon-b37d6",
     storageBucket: "tngon-b37d6.firebasestorage.app",
     messagingSenderId: "580319242104",
-    appId: "1:580319242104:web:6922e4327bdc8286c30a8d"
+    appId: "1:580319242104:web:6922e4327bdc8286c30a8d",
+    measurementId: "G-LHEH8ZC6SL"
   };
 
-  // Init 1 lần
-  if (!firebase.apps || !firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
+  if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
 
-  // Đăng nhập ẩn danh
-  const authP = firebase.auth().currentUser
-    ? Promise.resolve(firebase.auth().currentUser)
-    : firebase.auth().signInAnonymously().catch((e) => {
-        console.warn("[firebase.js] signInAnonymously failed:", e);
-      });
-
-  // Xuất db sau khi auth xong + PHÁT SỰ KIỆN firebase-ready
-  window.initFirebase = (async () => {
-    try { await authP; } catch(_) {}
-    const db = firebase.database();
-    window.db = db;
-
-    try {
-      window.dispatchEvent(new CustomEvent('firebase-ready', { detail: { db } }));
-    } catch (_) {}
-
-    return db;
-  })();
+  firebase.auth().signInAnonymously().catch((e)=>{
+    console.error("Firebase anonymous auth failed:", e);
+  });
 })();
