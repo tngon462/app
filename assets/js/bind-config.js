5022e4f1711c621b151bd54b2671b84b5afa31f9
tngon462 <tngon462@gmail.com>
2025-10-20 10:38:20 +0900

    Create bind-config.js

---
+++ assets/js/bind-config.js
@@ -0,0 +1,28 @@
+// bind-config.js
+// Hằng số + key lưu trữ + path DB cho phía iPad
+
+(function () {
+  window.TNGON = window.TNGON || {};
+
+  TNGON.bindCfg = {
+    // localStorage keys
+    lsKeys: {
+      deviceId: 'deviceId',
+      deviceCode: 'deviceCode',
+      appState: 'appState',      // 'select' | 'start' | 'pos' (do redirect-core.js ghi)
+      tableId: 'tableId',
+      tableUrl: 'tableUrl',
+      forceStartAfterReload: 'forceStartAfterReload',
+      handledReloadAt: 'handledReloadAt',
+      handledBroadcastAt: 'handledBroadcastAt',
+    },
+    // db paths
+    paths: {
+      codes: 'codes',
+      devices: 'devices',
+      broadcast: 'broadcast/reloadAt',
+    },
+    // timings
+    heartbeatMs: 20000,
+  };
+})();
